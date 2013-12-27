3f7db521662d6d4576410e99fb9e3ebc698af755
Inho Yong <bbung24@gmail.com>
2013-12-26 21:38:52 -0500

    Fixed You must set config.secret_key_base in your app's config. problem from heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HannahWebsite::Application.config.secret_key_base = 'c8e3d59653bd8331b3b91d564e80939f8ce9a5ea1912fb5d0fdcb9c139fbe832b41593ad1fb4718387328037eb9f121fcc0b3ad2974abaeac99c56a4f2d2274a'
